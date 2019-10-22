216c3e348b4e38f95bea8e2951209bc8f662d9cd
Kateryna Strough <katerynastrough@hotmail.com>
2019-10-22 23:08:26 +0000

    Done.

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,7 +1,19 @@
-class WelcomeController < ApplicationController
-  def home
-    if session[:user_id]
-      @user = User.find(session[:user_id])
+class SessionsController < ApplicationController
+  def create
+    @user = User.find_or_create_by(uid: auth['uid']) do |u|
+      u.name = auth['info']['name']
+      u.email = auth['info']['email']
+      u.image = auth['info']['image']
     end
+
+    session[:user_id] = @user.id
+
+    render 'welcome/home'
+  end
+
+  private
+
+  def auth
+    request.env['omniauth.auth']
   end
 end
